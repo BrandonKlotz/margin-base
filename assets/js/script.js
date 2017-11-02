3eeae4c160da27ac7a4da572c37f0ff5ec78cd85
HollyJHuber <hollyjhuber@gmail.com>
2017-11-02 09:50:48 -0400

    make item clickable, add ID

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,49 +2,51 @@
 $(function() {
 
 	const items = [
-	{ id: 10, name: "Alaskan Thunder", description: "dank weed", price: 1, category: "smokeables"},
-	{ id: 11, name: "Purple Haze", description: "skunk weed", price: 1, category: "smokeables"},
-	{ id: 12, name: "Pineapple Express", description: "not-so-dank weed", price: 1, category: "smokeables"},
-	{ id: 13, name: "Maui Wowie", description: "island weed", price: 1, category: "smokeables"},
-	{ id: 14, name: "Kingdom of Kush", description: "sweet weed", price: 1, category: "smokeables"},
-	{ id: 15, name: "After School Special", description: "not-so-skunky weed", price: 1, category: "smokeables"},
+	{ id: 10, name: "Alaskan Thunder", description: "dank weed", price: 10.00, category: "smokeables"},
+	{ id: 11, name: "Purple Haze", description: "skunk weed", price: 10.00, category: "smokeables"},
+	{ id: 12, name: "Pineapple Express", description: "not-so-dank weed", price: 10.00, category: "smokeables"},
+	{ id: 13, name: "Maui Wowie", description: "island weed", price: 10.00, category: "smokeables"},
+	{ id: 14, name: "Kingdom of Kush", description: "sweet weed", price: 10.00, category: "smokeables"},
+	{ id: 15, name: "After School Special", description: "not-so-skunky weed", price: 10.00, category: "smokeables"},
 
-	{ id: 50, name: "Ganja Gummies", description: "marijuana-infused gummy candy", price: 1, category: "edibles"},
-	{ id: 51, name: "Medicinal Brownies", description: "weed-laced brownies", price: 1, category: "edibles"},
-	{ id: 52, name: "Green Elixir", description: "bright green grass drink for sipping", price: 1, category: "edibles"},
-	{ id: 53, name: "No-Bake Cookies", description: "get baked with our no-bakes", price: 1, category: "edibles"},
+	{ id: 50, name: "Ganja Gummies", description: "marijuana-infused gummy candy", price: 10.00, category: "edibles"},
+	{ id: 51, name: "Medicinal Brownies", description: "weed-laced brownies", price: 10.00, category: "edibles"},
+	{ id: 52, name: "Green Elixir", description: "bright green grass drink for sipping", price: 10.00, category: "edibles"},
+	{ id: 53, name: "No-Bake Cookies", description: "get baked with our no-bakes", price: 10.00, category: "edibles"},
 
-	{ id: 70, name: "Rainbow Bowl", description: "smoke-a-bowl", price: 1, category: "accessories"},
-	{ id: 71, name: "Grinder not grindr", description: "grind it up", price: 1, category: "accessories"}
+	{ id: 70, name: "Rainbow Bowl", description: "smoke-a-bowl", price: 10.00, category: "accessories"},
+	{ id: 71, name: "Grinder not grindr", description: "grind it up", price: 10.00, category: "accessories"}
 	];	
 
 	const shoppingCart = [];
 
 	// insert items into index.html
 	function listItems() {
-		let $wrapper = $('.wrapper');
+		let wrapper = $('.wrapper');
 		let currentCategory = "";
 	    items.forEach(function(item) {   
 	    	// insert category div if necessary
 	    	if( item.category !== currentCategory){
-	    		let $category = '<div class="categoryHead">' + item.category.toUpperCase() + '</div>';
-	    		$wrapper.append($category);
+	    		let category = '<div class="categoryHead">' + item.category.toUpperCase() + '</div>';
+	    		wrapper.append(category);
 	    		currentCategory = item.category;
 	    	}
-	    	let $menuItem = '<div class="item">' + item.name + '&nbsp;<button class="addToCart" type="button"> + </button></div>';
-	    	$wrapper.append($menuItem);
+	    	let menuItem = '<div id="' + item.id + '" class="item">' + item.name + '</div>';
+
+	    	// let menuItem = '<div class="item">' + item.name + '&nbsp;<button class="addToCart" type="button"> + </button></div>';
+	    	wrapper.append(menuItem);
 	    });
-	    addButtons();
+	    // you have to call the addButtons function after they've been created!
+	    addClick();
 	  }
 
-	  function addButtons() {
-	  		$('button.addToCart').on('click', function(){
-				console.log("clicked it!");
+	  function addClick() {
+	  		$('div.item').on('click', function(){
+				console.log("clicked it");
 				$(this).addClass('selected');
-				// where this refers to the button, not the item
 				// how do we push the item data to the shoppingCart array?
-
-				//let selectedItem = $(this).item.name;
+				//let selectedItem = $(this).id;
+				//console.log(selectedItem);
 			});
 	  }
 
